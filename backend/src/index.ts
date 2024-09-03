5e58d693da98559fb50b8681d95689234723b445
Vezil <szymonwojaczek@wp.pl>
2024-09-03 23:17:25 +0200

    add todolist and styling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,14 +3,39 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+interface Todo {
+    id: number;
+    text: string;
+    isDone: boolean;
+}
+
+const todos: Todo[] = [];
+
+todos.push({
+    id: 0,
+    text: 'start',
+    isDone: false
+});
+
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     next();
 });
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
+app.get('/get-todos', (req, res) => {
+    res.send(todos);
+});
+
+app.post('/add-todo', (req, res) => {
+    const { text } = req.body;
+
+    todos.push({ id: todos.length, text, isDone: false });
+
+    res.status(201).end();
 });
 
 app.listen(port, () => {
--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,27 +1,86 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useCallback, useState } from 'react';
+import { StyleSheet, Text, View, TextInput, SafeAreaView, SectionList, Button } from 'react-native';
 import axios from 'axios';
 
+interface Todo {
+    id: number;
+    text: string;
+    isDone: boolean;
+}
+
 export default function App() {
-    const [message, setMessage] = useState<string>('');
+    const [userTodo, setUserTodo] = useState<Todo>({
+        id: 0,
+        text: '',
+        isDone: false
+    });
+
+    const [allTodos, setTodos] = useState<{ title: string; data: Todo[] }[]>([]);
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get<Todo[]>('http://localhost:3000/get-todos');
+
+            const todos = response.data;
+
+            const sections = [
+                {
+                    title: 'Todos',
+                    data: todos.filter(todo => !todo.isDone) // incomplete todos
+                },
+                {
+                    title: 'Completed',
+                    data: todos.filter(todo => todo.isDone) // completed todos
+                }
+            ];
+
+            setTodos(sections);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('http://localhost:3000/hello');
-
-                setMessage(response.data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
         fetchData();
     }, []);
 
+    const onConfirm = async () => {
+        await axios.post('http://localhost:3000/add-todo', { text: userTodo.text });
+
+        fetchData();
+    };
+
+    const ToDoList = ({ allTodos }) => {
+        if (!allTodos.length) {
+            return <Text>No todos available</Text>;
+        }
+
+        return (
+            <SafeAreaView style={styles.safeAreaViewContainer}>
+                <SectionList
+                    sections={allTodos}
+                    keyExtractor={item => item.id.toString()}
+                    renderItem={({ item }) => (
+                        <View style={styles.item}>
+                            <Text style={styles.title}>{item.text}</Text>
+                        </View>
+                    )}
+                    renderSectionHeader={({ section: { title } }) => <Text style={styles.header}>{title}</Text>}
+                />
+            </SafeAreaView>
+        );
+    };
+
     return (
         <View style={styles.container}>
-            <Text>{message}</Text>
+            <ToDoList allTodos={allTodos} />
+            <TextInput
+                value={userTodo.text}
+                onChangeText={text => setUserTodo({ ...userTodo, text })}
+                style={styles.addTodoInput}
+            />
+
+            <Button onPress={onConfirm} title="Add Todo" color="#841584" accessibilityLabel="Add your new Todo" />
         </View>
     );
 }
@@ -32,5 +91,25 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    safeAreaViewContainer: {
+        backgroundColor: '#eee'
+    },
+    title: {
+        fontSize: 24
+    },
+    header: {
+        fontSize: 32,
+        backgroundColor: '#fff'
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8
+    },
+    addTodoInput: {
+        backgroundColor: '#ccc',
+        padding: 10,
+        marginBottom: 4
     }
 });
